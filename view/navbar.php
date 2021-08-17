a71fad9dfc3b37528b7acf4a50006800cab4beb5
luan.sabino <luanphy.y@gmail.com>
2021-08-17 16:07:18 -0300

    Criado o modelo de NavBar, e o css principal.

---
+++ view/navbar.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link rel="stylesheet" href="colors.css">
+    <title></title>
+</head>
+<body>
+    <nav class="navbar navbar-light bg-color">
+        <div class="dropdown">
+            <button class="btn btn-primary btn-lg btn-bg-color" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i class="fas fa-bars"></i>
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+                <button class="dropdown-item" type="button">Item 1</button>
+                <button class="dropdown-item" type="button">Item 2</button>
+                <button class="dropdown-item" type="button">Item 3</button>
+            </div>
+        </div>
+        <a class="navbar-brand brand-pos" href="#">
+            <img src="https://lh3.googleusercontent.com/proxy/CJ-gdbswPV8_ymdQVnQ6mY4HxdGUcFFrEOGYoy0b3AUr_WsLWoPR1o2r7-28Rcc4oL0_81QV0FGZbMVWQpgYU9yeaRPzNPSByLutYyPMCcLjSBWJaO14rJvcS_IHESwLkykS1zvrp8WOC5s" width="106" height="43" class="rounded mx-auto d-block" alt="">
+        </a>
+        <di class="btn-toolbar" role="toolbar">
+            <div class="btn-group mr-2" role="group" aria-label="Notifications">
+                <button class="btn btn-secondary btn-lg" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <i class="fas fa-bell" width="100" height="100"></i>
+                </button>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+                    <button class="dropdown-item" type="button">Item 1</button>
+                    <button class="dropdown-item" type="button">Item 2</button>
+                    <button class="dropdown-item" type="button">Item 3</button>
+                </div>
+            </div>
+            <div class="btn-group" role="group" aria-label="actions">
+                <button type="button" class="btn btn-primary btn-lg btn-bg-color">Logout</button>
+            </div>
+        </div>
+    </nav>
+</body>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+<script src="https://kit.fontawesome.com/1eaa5ac35a.js" crossorigin="anonymous"></script>
+</html>
