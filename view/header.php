a46369b3fc9d959a459063ffc09625cca92d9378
luan.sabino <luanphy.y@gmail.com>
2021-08-23 15:20:43 -0300

    Resolvido erro de cache no css

--- view/header.php
+++ view/header.php
@@ -80,7 +80,7 @@ elseif($_SESSION['tipo_usuario'] == 'erro')
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-    <link rel="stylesheet" href="../styles/main.css">
+    <link rel="stylesheet" href="../styles/main.css?v=<?php echo time(); ?>">
     <title></title>
 </head>
 <body>
