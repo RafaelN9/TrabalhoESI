d307eae0edf5ad2192e47d31b54787096dc29df2
luan.sabino <luanphy.y@gmail.com>
2021-08-26 11:17:59 -0300

    Exemplo de teste com phpunit, e composer cfg

---
+++ public/calculadora.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+final class Calculadora{
+
+    public static function soma($a, $b) {return $a+$b;}
+
+}
+
+?>
---
+++ test/CalculadoraTest.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+require __DIR__ . "/../public/calculadora.php";
+
+final class CalculadoraTest extends PHPUnit\Framework\TestCase{
+
+    public function testaSomaDaCalculadora(): void{
+        $calculadora = new Calculadora();
+        $this->assertEquals(3, $calculadora::soma(1,2));
+    }
+
+}
+
+?>
