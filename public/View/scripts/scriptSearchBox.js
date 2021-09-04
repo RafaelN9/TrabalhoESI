6b90b66bc3824c9c0b7c9b009e6cfa0526fe0bbd
luan.sabino <luanphy.y@gmail.com>
2021-09-04 20:20:58 -0300

    Filtro agora funciona com nome de professor

--- public/View/scripts/scriptSearchBox.js
+++ public/View/scripts/scriptSearchBox.js
@@ -1,3 +1,9 @@
+var ehCCP = false;
+
+if(document.querySelector('#head').children[3] != undefined){
+    ehCCP = true;
+}
+
 var searchboxElement = document.querySelector("#search-box");
 searchboxElement.addEventListener('keyup', ()=>{
 
@@ -5,16 +11,31 @@ searchboxElement.addEventListener('keyup', ()=>{
     value = value.toUpperCase();
 
     let allRows = document.getElementsByTagName('tr');
+
     for(element of allRows){
         if(element.id == 'head') continue;
         element.style['display'] = 'none';
-        let name = element.children[1].textContent;
-        name = name.toUpperCase();
-        console.log(name);
-        console.log(value);
-        if(name.includes(value)){
-            element.style['display'] = 'table-row';
+        
+        let nomeAluno = element.children[1].textContent;
+        nomeAluno = nomeAluno.toUpperCase();
+
+        switch (ehCCP) {
+            case true:
+                let nomeProfessor = element.children[3].textContent;
+                nomeProfessor = nomeProfessor.toUpperCase();
+
+                if(nomeAluno.includes(value) || nomeProfessor.includes(value)){
+                    element.style['display'] = 'table-row';
+                }
+                break;
+        
+            default:
+                if(nomeAluno.includes(value)){
+                    element.style['display'] = 'table-row';
+                }
+                break;
         }
+
     }
 
 });
