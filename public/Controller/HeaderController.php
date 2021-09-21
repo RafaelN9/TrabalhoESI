8254d0e75be848ad834a1946e6d5217a967ef39e
luan.sabino <luanphy.y@gmail.com>
2021-09-21 14:22:43 -0300

    Concatenação dos controllers de notificação

--- public/Controller/HeaderController.php
+++ public/Controller/HeaderController.php
@@ -67,7 +67,7 @@ class HeaderController{
         }
 
         if($this->dropNotifica == ''){
-            $notificaController = new MostraNotificaController($notificacoes);
+            $notificaController = new NotificacaoController($notificacoes);
             $numNotifica = $notificaController->getNumNotifications();
             $notificaContent = $notificaController->getNotificationsContent();
             $numNotificaContent = $numNotifica > 0 ? "<div class='rounded-circle bg-danger mt-1 ml-1' style='width: 20px; height: 20px; font-size: 12px;'>$numNotifica</div>" :
--- public/Controller/NotificacaoController.php
+++ public/Controller/NotificacaoController.php
@@ -4,6 +4,55 @@ require_once 'DBServices/insertBD.php';
 
 class NotificacaoController
 {
+    private $notifications = [];
+    private $numNotifications = 0;
+    private $notificationsContent = "<p class='p-5 mx-auto my-auto'>Sem notificações</p>";
+
+    public function __construct()
+    {
+        $arguments = func_get_args();
+        $numberOfArguments = func_num_args();
+
+        if (method_exists($this, $function = '__construct'.$numberOfArguments)) {
+            call_user_func_array(array($this, $function), $arguments);
+        }
+    }
+
+    function __construct0(){}
+
+    function __construct1($notificacoes){
+        $this->notifications = $notificacoes;
+
+        function addNotification(Notificacao $notification, $index){
+            global $numNotifications;
+            $numNotifications++;
+            $notificaLink = $notification->getLink();
+            $notificaTexto = $notification->getTexto();
+            $notificaData = $notification->getData();
+            $cor = 'alert-'.$notification->getCor();
+            return "
+                <div class='d-flex flex-colunm dropdown-item $cor' id='$index'>
+                    <div class='dropdown-item px-0 text-wrap'  onclick='Redireciona(`$notificaLink`)' > $notificaTexto
+                        <br>
+                        <span class='text-muted' style='font-size: 12px;'>$notificaData</span>
+                    </div>
+                    <button type='button' class='close pr-1' data-dismiss='alert' aria-label='Close' onclick='deleteNotify($index)'>
+                        <span aria-hidden='true'>&times;</span>
+                    </button>
+                </div>";
+
+        }
+
+        if($this->notifications != "erro"){
+            $this->notificationsContent = '';
+            foreach($this->notifications as $elem){
+                $notif = new Notificacao($elem['usuario'], $elem["texto"], $elem["link"], $elem['cor']);
+                $notif->setCodigo($elem['codigo']);
+                $notif->setData($elem['data']);
+                $this->notificationsContent .= addNotification($notif, $elem['codigo']);
+            }
+        }
+    }
 
     public function adicionaNotificacaoAluno($usuario, $texto, $link , $cor){
         $notificacao = new Notificacao($usuario, $texto, $link, $cor);
@@ -31,62 +80,20 @@ class NotificacaoController
         $bd = new NotificacoesService();
         $resultado = $bd->getNotificacaoAluno($usuario);
         return $resultado;
-    } 
+    }
+
     function getNotificacoesProfessor($usuario){
         require_once 'DBServices/NotificacoesService.php';
         $bd = new NotificacoesService();
         $resultado = $bd->getNotificacaoProfessor($usuario);
         return $resultado;
-    } 
+    }
+
     function getNotificacoesCCP($usuario){
         require_once 'DBServices/NotificacoesService.php';
         $bd = new NotificacoesService();
         $resultado = $bd->getNotificacaoCCP($usuario);
         return $resultado;
-    }
-    
-
-}
-
-class MostraNotificaController{
-    //Exibição das notificações
-
-    private $notifications = [];
-    private $numNotifications = 0;
-    private $notificationsContent = "<p class='p-5 mx-auto my-auto'>Sem notificações</p>";
-
-    function __construct($notificacoes){
-        $this->notifications = $notificacoes;
-
-        function addNotification(Notificacao $notification, $index){
-            global $numNotifications;
-            $numNotifications++;
-            $notificaLink = $notification->getLink();
-            $notificaTexto = $notification->getTexto();
-            $notificaData = $notification->getData();
-            $cor = 'alert-'.$notification->getCor();
-            return "
-                <div class='d-flex flex-colunm dropdown-item $cor' id='$index'>
-                    <div class='dropdown-item px-0 text-wrap'  onclick='Redireciona(`$notificaLink`)' > $notificaTexto
-                        <br>
-                        <span class='text-muted' style='font-size: 12px;'>$notificaData</span>
-                    </div>
-                    <button type='button' class='close pr-1' data-dismiss='alert' aria-label='Close' onclick='deleteNotify($index)'>
-                        <span aria-hidden='true'>&times;</span>
-                    </button>
-                </div>";
-    
-        }
-
-        if($this->notifications != "erro"){
-            $this->notificationsContent = '';
-            foreach($this->notifications as $elem){
-                $notif = new Notificacao($elem['usuario'], $elem["texto"], $elem["link"], $elem['cor']);
-                $notif->setCodigo($elem['codigo']);
-                $notif->setData($elem['data']);
-                $this->notificationsContent .= addNotification($notif, $elem['codigo']);
-            }
-        }
     }
 
     function getNotificationsContent(){
@@ -95,4 +102,6 @@ class MostraNotificaController{
     function getNumNotifications(){
         return $this->numNotifications;
     }
+    
+
 }
